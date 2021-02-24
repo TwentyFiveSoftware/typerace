6cb0605d67b865887ef58cdfa56c467ebf5433bd
Sebastian Loose <sebastian.loose@yahoo.de>
2021-02-24 19:29:40 +0100

    add lobby not found label

--- src/components/JoinLobby.tsx
+++ src/components/JoinLobby.tsx
@@ -3,17 +3,22 @@ import Container from './Container';
 import InputBox from './InputBox';
 import Button from './Button';
 import { socket } from '../App';
+import styles from '../styles/JoinLobby.module.scss';
 
 const JoinLobby = () => {
     const [username, setUsername] = useState<string>('');
     const [lobbyId, setLobbyId] = useState<string>('');
+    const [lobbyNotFound, setLobbyNotFound] = useState<boolean>(false);
 
     const submit = () => socket.emit('joinLobby', username, lobbyId);
+
+    socket.on('lobbyNotFound', () => setLobbyNotFound(true));
 
     return (
         <Container>
             <InputBox label={'USERNAME'} value={username} onChange={setUsername} />
             <InputBox label={'LOBBY ID (EMPTY FOR NEW LOBBY)'} value={lobbyId} onChange={setLobbyId} />
+            {lobbyNotFound && <p className={styles.lobbyNotFoundLabel}>Lobby Not Found</p>}
             <Button text={'JOIN'} onClick={() => submit()} />
         </Container>
     );
