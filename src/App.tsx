951415fadcbb0ddb939b53e626b3dc292004571b
TwentyFive <TwentyFiveSoftware@gmail.com>
2022-02-05 14:12:42 +0100

    remove header

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import styles from './styles/App.module.scss';
 import type { LobbyState } from './type/LobbyState';
 import type { GameState } from './type/GameState';
 import { SocketResponseType } from './type/SocketResponseType';
-import Header from './components/Header';
 import JoinLobby from './components/JoinLobby';
 import Lobby from './components/Lobby';
 import Game from './components/Game';
@@ -14,7 +13,7 @@ export const socket = io(process.env.REACT_APP_SOCKET_ENDPOINT ?? '');
 export const GameStateContext = createContext<GameState | null>(null);
 export const LobbyStateContext = createContext<LobbyState | null>(null);
 
-const App = () => {
+const App: React.FC = () => {
     const [lobbyState, setLobbyState] = useState<LobbyState | null>(null);
     const [gameState, setGameState] = useState<GameState | null>(null);
 
@@ -25,22 +24,18 @@ const App = () => {
 
     console.log(gameState);
 
-    return gameState ? (
+    return (
         <div className={styles.page}>
-            <Header />
-
-            <GameStateContext.Provider value={gameState}>
-                <Game />
-            </GameStateContext.Provider>
-        </div>
-    ) : (
-        <div className={styles.page}>
-            <Header />
-            {!lobbyState && <JoinLobby />}
-            {lobbyState && (
+            {!gameState && !lobbyState && <JoinLobby />}
+            {!gameState && lobbyState && (
                 <LobbyStateContext.Provider value={lobbyState}>
                     <Lobby />
                 </LobbyStateContext.Provider>
+            )}
+            {gameState && (
+                <GameStateContext.Provider value={gameState}>
+                    <Game />
+                </GameStateContext.Provider>
             )}
         </div>
     );
